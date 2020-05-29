9d166cf184c5b3ce6c45f1111937a8070a9d8a06
Francisco Fernández <ferrycosv@gmail.com>
2020-05-29 20:53:31 +0200

    All completed!

--- 2-sync-to-async/exercise-2-async-await.js
+++ 2-sync-to-async/exercise-2-async-await.js
@@ -1,18 +1,16 @@
 // require dependencies
-const fs = require('fs');
-const path = require('path');
-const assert = require('assert');
+const fs = require("fs");
+const path = require("path");
+const assert = require("assert");
+const util = require("util");
 
 // declare constants
 const EXERCISE_NAME = path.basename(__filename);
 const START = Date.now();
 
 // declare logging function
-const log = (logId, value) => console.log(
-  `\nlog ${logId} (${Date.now() - START} ms):\n`,
-  value,
-);
-
+const log = (logId, value) =>
+  console.log(`\nlog ${logId} (${Date.now() - START} ms):\n`, value);
 
 // --- main script ---
 console.log(`\n--- ${EXERCISE_NAME} ---`);
@@ -26,32 +24,43 @@ const filePath2 = path.join(__dirname, fileName2);
 log(2, filePath2);
 
 log(3, `reading ${fileName1} ...`);
-const fileContents1 = fs.readFileSync(filePath1, 'utf-8');
-
 log(4, `reading ${fileName2} ...`);
-const fileContents2 = fs.readFileSync(filePath2, 'utf-8');
 
-log(5, 'comparing file contents ...');
-const fileOneIsLonger = fileContents1.length > fileContents2.length;
-if (fileOneIsLonger) {
-  log(6, `writing to ${fileName2} ...`);
-  fs.writeFileSync(filePath2, fileContents1);
-} else {
-  log(6, `writing to ${fileName1} ...`);
-  fs.writeFileSync(filePath1, fileContents2);
+const readFilePromise = util.promisify(fs.readFile);
+const writeFilePromise = util.promisify(fs.writeFile);
+
+const main = async () => {
+  try {
+    const readFiles = await Promise.all([
+      readFilePromise(filePath1, `utf-8`),
+      readFilePromise(filePath2, `utf-8`),
+    ]);
+    log(6, "comparing file contents ...");
+    const fileOneIsLonger = readFiles[0].length > readFiles[1].length;
+    if (fileOneIsLonger) {
+      log(6, `writing to ${fileName2} ...`);
+      await writeFilePromise(filePath2, readFiles[0]);
+    } else {
+      log(6, `writing to ${fileName1} ...`);
+      await writeFilePromise(filePath1, readFiles[1]);
+    }
+    if (fileOneIsLonger) {
+      log(7, `reading ${fileName2} ...`);
+      const newFileContents2 = await readFilePromise(filePath2, "utf-8");
+      log(8, "asserting ...");
+      assert.strictEqual(readFiles[0], newFileContents2);
+    } else {
+      log(7, `reading ${fileName1} ...`);
+      const newFileContents1 = await readFilePromise(filePath1, "utf-8");
+      log(8, "asserting ...");
+      assert.strictEqual(readFiles[1], newFileContents1);
+    }
+    log(9, "\033[32mpass!\x1b[0m");
+    fs.appendFileSync(__filename, `\n// pass: ${new Date().toLocaleString()}`);
+  } catch (err) {
+    console.error(err);
+  }
 };
+main();
 
-if (fileOneIsLonger) {
-  log(7, `reading ${fileName2} ...`);
-  const newFileContents2 = fs.readFileSync(filePath2, 'utf-8');
-  log(8, 'asserting ...');
-  assert.strictEqual(fileContents1, newFileContents2);
-} else {
-  log(7, `reading ${fileName1} ...`);
-  const newFileContents1 = fs.readFileSync(filePath1, 'utf-8');
-  log(8, 'asserting ...');
-  assert.strictEqual(fileContents2, newFileContents1);
-};
-
-log(9, '\033[32mpass!\x1b[0m');
-fs.appendFileSync(__filename, `\n// pass: ${(new Date()).toLocaleString()}`);
+// pass: 5/29/2020, 8:09:28 PM
--- 2-sync-to-async/exercise-3-async-await.js
+++ 2-sync-to-async/exercise-3-async-await.js
@@ -1,18 +1,16 @@
 // require dependencies
-const fs = require('fs');
-const path = require('path');
-const assert = require('assert');
+const fs = require("fs");
+const path = require("path");
+const assert = require("assert");
+const util = require("util");
 
 // declare constants
 const EXERCISE_NAME = path.basename(__filename);
 const START = Date.now();
 
 // declare logging function
-const log = (logId, value) => console.log(
-  `\nlog ${logId} (${Date.now() - START} ms):\n`,
-  value,
-);
-
+const log = (logId, value) =>
+  console.log(`\nlog ${logId} (${Date.now() - START} ms):\n`, value);
 
 // --- main script ---
 console.log(`\n--- ${EXERCISE_NAME} ---`);
@@ -26,21 +24,31 @@ log(2, toAppend);
 const numberOfTimes = Number(process.argv[4]);
 log(3, numberOfTimes);
 
-log(4, 'reading old contents ...');
-const oldContents = fs.readFileSync(filePath, 'utf-8');
+log(4, "reading old contents ...");
 
-for (let i = 1; i <= numberOfTimes; i++) {
-  log(4 + i, `appending ...`);
-  fs.appendFileSync(filePath, toAppend);
+const readFilePromise = util.promisify(fs.readFile);
+const appendFilePromise = util.promisify(fs.appendFile);
+
+const main = async () => {
+  try {
+    const oldContents = await readFilePromise(filePath, "utf-8");
+    for (let i = 1; i <= numberOfTimes; i++) {
+      log(4 + i, `appending ...`);
+      await appendFilePromise(filePath, toAppend);
+    }
+    log(numberOfTimes + 5, "reading new contents ...");
+    const newContents = await readFilePromise(filePath, "utf-8");
+
+    log(numberOfTimes + 6, "asserting file contents ...");
+    const expectedContents = oldContents + toAppend.repeat(numberOfTimes);
+    assert.strictEqual(newContents, expectedContents);
+
+    log(numberOfTimes + 7, "\033[32mpass!\x1b[0m");
+    fs.appendFileSync(__filename, `\n// pass: ${new Date().toLocaleString()}`);
+  } catch (err) {
+    console.error(err);
+  }
 };
+main();
 
-log(numberOfTimes + 5, 'reading new contents ...');
-const newContents = fs.readFileSync(filePath, 'utf-8');
-
-log(numberOfTimes + 6, 'asserting file contents ...');
-const expectedContents = oldContents + toAppend.repeat(numberOfTimes);
-assert.strictEqual(newContents, expectedContents);
-
-log(numberOfTimes + 7, '\033[32mpass!\x1b[0m');
-fs.appendFileSync(__filename, `\n// pass: ${(new Date()).toLocaleString()}`);
-
+// pass: 5/29/2020, 8:26:07 PM
